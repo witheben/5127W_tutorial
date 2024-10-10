076472f6b8ad1f7b0f8b2065c92bb8be01174f71
ben withey <withe096@umn.edu>
2024-10-10 18:21:33 -0500

    updates

--- recognizer.py
+++ recognizer.py
@@ -6,9 +6,11 @@ from tqdm import tqdm
 
 highest_score = 0
 image = ""
-for i in tqdm(os.listdir("references/d12-v3")):
-    img1 = cv.imread(os.path.join("references/d12-v3", i), cv.IMREAD_GRAYSCALE)          # queryImage
-    img2 = cv.imread('test_inputs/d12-v2/4.jpg', cv.IMREAD_GRAYSCALE) # trainImag
+ref_images_path = "references/d12-v3"
+query_image = "test_inputs/d12-v2/4.jpg"
+for i in tqdm(os.listdir(ref_images_path)):
+    img1 = cv.imread(os.path.join(ref_images_path, i), cv.IMREAD_GRAYSCALE)
+    img2 = cv.imread(query_image, cv.IMREAD_GRAYSCALE)
     
     # Initiate ORB detector
     orb = cv.ORB_create()
@@ -17,18 +19,17 @@ for i in tqdm(os.listdir("references/d12-v3")):
     kp1, des1 = orb.detectAndCompute(img1,None)
     kp2, des2 = orb.detectAndCompute(img2,None)
 
-
     # create BFMatcher object
     bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
     
     # Match descriptors.
     matches = bf.match(des1,des2)
     
-    # Sort them in the order of their distance.
-    matches = sorted(matches, key = lambda x:x.distance)
+    # # Sort them in the order of their distance.
+    # matches = sorted(matches, key = lambda x:x.distance)
     
-    # Draw first 10 matches.
-    img3 = cv.drawMatches(img1,kp1,img2,kp2,matches,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # # Draw first 10 matches.
+    # img3 = cv.drawMatches(img1,kp1,img2,kp2,matches,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
     if len(matches) > highest_score:
         highest_score = len(matches)
