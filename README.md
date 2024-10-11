2aa03450aa7f4cdb56c31e9378c2ff5f7b5c281f
Benjamin Withey <withe096.umn.edu>
2024-10-10 19:11:11 -0500

    intermediate step

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 5127W_tutorial
+
+## TODO
+
+1. Implement capturing image when button in tkinter is clicked and run the detector
+2. Finish testing the d20 and d12
+    - Test out ORB and SIFT feature matching (currently using ORB)
+3. Add the math to the 
+4. Make a better dice box (maybe order a dice tower)
--- recognizer.py
+++ recognizer.py
@@ -1,42 +1,90 @@
-import numpy as np
-import cv2 as cv
+import cv2
 import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
 
-highest_score = 0
-image = ""
-ref_images_path = "references/d12-v3"
-query_image = "test_inputs/d12-v2/4.jpg"
-for i in tqdm(os.listdir(ref_images_path)):
-    img1 = cv.imread(os.path.join(ref_images_path, i), cv.IMREAD_GRAYSCALE)
-    img2 = cv.imread(query_image, cv.IMREAD_GRAYSCALE)
-    
-    # Initiate ORB detector
-    orb = cv.ORB_create()
-    
-    # find the keypoints and descriptors with ORB
-    kp1, des1 = orb.detectAndCompute(img1,None)
-    kp2, des2 = orb.detectAndCompute(img2,None)
+import tkinter as tk
 
-    # create BFMatcher object
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-    
-    # Match descriptors.
-    matches = bf.match(des1,des2)
-    
-    # # Sort them in the order of their distance.
-    # matches = sorted(matches, key = lambda x:x.distance)
-    
-    # # Draw first 10 matches.
-    # img3 = cv.drawMatches(img1,kp1,img2,kp2,matches,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+def dice_roll(ref_images_path, query_image):
+    highest_score = 0
+    image = ""
+    # ref_images_path = "references/d12-v3"
+    # query_image = "test_inputs/d12-v2/4.jpg"
+    for i in tqdm(os.listdir(ref_images_path)):
+        img1 = cv2.imread(os.path.join(ref_images_path, i), cv2.IMREAD_GRAYSCALE)
+        img2 = cv2.imread(query_image, cv2.IMREAD_GRAYSCALE)
+        
+        # Initiate ORB detector
+        orb = cv2.ORB_create()
+        
+        # find the keypoints and descriptors with ORB
+        kp1, des1 = orb.detectAndCompute(img1,None)
+        kp2, des2 = orb.detectAndCompute(img2,None)
 
-    if len(matches) > highest_score:
-        highest_score = len(matches)
-        image = i
-    # print()
-    # print(i)
-    # print(len(matches))
-    # plt.imshow(img3),plt.show()
-print(highest_score)
-print(image)
+        # create BFMatcher object
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        
+        # Match descriptors.
+        matches = bf.match(des1,des2)
+        
+        # # Sort them in the order of their distance.
+        # matches = sorted(matches, key = lambda x:x.distance)
+        
+        # # Draw first 10 matches.
+        # img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+        if len(matches) > highest_score:
+            highest_score = len(matches)
+            image = i
+        # print()
+        # print(i)
+        # print(len(matches))
+        # plt.imshow(img3),plt.show()
+    print(highest_score)
+    print(image)
+
+def run_GUI():
+    window = tk.Tk()
+    window.geometry("500x500")
+    window.title("DnD calculate dice roll")
+
+    
+
+    d_12_roll = tk.Button(
+    text="d12 Roll",
+    width=10,
+    height=5,
+    command=lambda: dice_roll("references/d12-v3")
+    )
+    d_20_roll = tk.Button(
+        text="d20 Roll",
+        width=10,
+        height=5,
+        command=lambda: dice_roll("references/d20")
+    )
+    d_12_roll.pack()
+    d_20_roll.pack()
+
+
+
+    
+    window.mainloop()
+    print("here")
+
+if __name__ == "__main__":
+    run_GUI()
+    exit(0)
+
+    cap = cv2.VideoCapture(0)
+
+
+
+    ret, frame = cap.read()
+    cv2.imshow("Webcam", frame)
+    cv2.waitKey(0)
+
+
+
+    # Release the camera and close the window
+    cap.release()
+    cv2.destroyAllWindows()
